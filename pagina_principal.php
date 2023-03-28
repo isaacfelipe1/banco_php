af29aec84610ebcba92071b7cff53b80b55dd678
isaacfelipe1 <isaacfylype18@gmail.com>
2023-03-28 18:28:46 -0300

    Update pagina_principal.php

--- pagina_principal.php
+++ pagina_principal.php
@@ -1,15 +1,19 @@
-<html>
-  <head>
-    <title>Login</title>
-  </head>
-  <body>
-    <h1>Login</h1>
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina principal</title>
+</head>
+<body>
+<h1>Login</h1>
     <form method="post" action="login.php">
       <label>Nome de Usuário:</label>
       <input type="text" name="nome"><br>
       <label>Senha:</label>
-      <input type="password" name="senha"><br>
+      <input type="p" name="senha"><br>
       <input type="submit" value="Entrar">
     </form>
-  </body>
+</body>
 </html>
